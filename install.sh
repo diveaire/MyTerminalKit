0ba470ff0442b5cb3bacdd691fab5206135651ba
Nicolas <n.auvray.na@gmail.com>
2024-01-11 20:34:13 +0100

    Ajout de l'installation des commandes de base

--- install.sh
+++ install.sh
@@ -34,20 +34,53 @@ install_zsh() {
     fi
 }
 
-# Installer la police MesloLGS NF
-install_font() {
-    if [ "$1" = "macos" ]; then
-        brew tap homebrew/cask-fonts
-        brew install --cask font-meslo-lg-nerd-font
-    elif [ "$1" = "linux" ]; then
-        # Pour Linux, le processus peut varier selon la distribution.
-        # Voici un exemple général pour télécharger et installer la police.
-        wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-        mkdir -p ~/.local/share/fonts
-        mv "MesloLGS NF Regular.ttf" ~/.local/share/fonts
-        fc-cache -f -v
+# Installer Ruby et Ruby Dev
+install_ruby() {
+    if ! command -v ruby >/dev/null 2>&1; then
+        echo "Installation de Ruby..."
+        if [ "$1" = "macos" ]; then
+            brew install ruby
+        elif [ "$1" = "linux" ]; then
+            sudo apt update
+            sudo apt install ruby ruby-dev
+        else
+            echo "Système d'exploitation non pris en charge."
+            exit 1
+        fi
+    else
+        echo "Ruby est déjà installé."
     fi
 }
+
+install_required_tools() {
+    echo "Vérification et installation des outils requis..."
+
+    # Liste des outils requis
+    required_tools=("unzip" "bat")
+
+    for tool in "${required_tools[@]}"; do
+        if ! command -v $tool &> /dev/null; then
+            echo "Installation de $tool..."
+
+            if [ "$1" = "Darwin" ]; then
+                # Installer avec Homebrew sur macOS
+                brew install $tool
+            elif [ "$1" = "Linux" ]; then
+                # Installer avec apt sur Linux
+                sudo apt-get install -y $tool
+            fi
+        else
+            echo "$tool est déjà installé."
+        fi
+    done
+}
+
+# Installer color-ls
+install_colorls() {
+    gem install colorls
+}
+
+
 
 # OS principal
 os=$(detect_os)
@@ -61,6 +94,16 @@ install_zsh $os
 # Installer Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# Installer Ruby et Ruby Dev
+install_ruby $os
+
+# Installer les paquets de bases
+install_required_tools $os
+
+
+# Installer color-ls
+install_colorls
 
 # Copier les fichiers de configuration
 cp path/to/your/.zshrc ~/
